1f4ca80780afac3a768d4c019998c9e0fa44ab2a
Paul Shryock <paul@pshry.com>
2020-12-11 07:32:50 -0500

    #2: Lint HTML (#14)

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const merge = require('merge-stream')
 const del = require('del')
 const Eleventy = require('@11ty/eleventy')
 const ssg = new Eleventy()
+const htmllint = require('gulp-htmllint')
 const beautify = require('gulp-beautify')
 const htmlmin = require('gulp-htmlmin')
 const connect = require('gulp-connect')
@@ -30,39 +31,41 @@ const isProduction = config.get('build.environment') === 'production'
  * @type {Object}
  */
 const paths = {
-  src: './src/pshry.com',
-  build: './build/pshry.com',
-  changelog: './CHANGELOG.md',
-  get html () {
-    return {
-      written: `${this.build}/**/*.html`
-    }
-  },
-  javascript: {
-    config: './config/*.js',
-    src: './src/**/*.js',
-    root: {
-      files: './*.js',
-      dotfiles: './.*.js',
-      get all () {
-        return [
-          this.files,
-          this.dotfiles
-        ]
-      }
-    },
-    get lint () {
-      return [
-        this.config,
-        this.src,
-        this.root.files
-      ]
-    }
-  }
+	build: './build/pshry.com',
+	changelog: './CHANGELOG.md',
+	html: {
+		src: './src/**/*.html',
+		get lint () {
+			return this.src
+		},
+		written: './build/**/*.html',
+		dest: './build'
+	},
+	javascript: {
+		config: './config/*.js',
+		src: './src/**/*.js',
+		root: {
+			files: './*.js',
+			dotfiles: './.*.js',
+			get all () {
+				return [
+					this.files,
+					this.dotfiles
+				]
+			}
+		},
+		get lint () {
+			return [
+				this.config,
+				this.src,
+				this.root.files
+			]
+		}
+	}
 }
 
 /**
- * Cleans the build directory.
+ * Clean the build directory.
  * Usage: `gulp clean`
  *
  * @since unreleased
@@ -70,9 +73,9 @@ const paths = {
  * @return {Promise}
  */
 exports.clean = function clean () {
-  return Promise.all([
-    del([paths.build])
-  ])
+	return Promise.all([
+		del([paths.html.dest])
+	])
 }
 
 /**
@@ -84,17 +87,25 @@ exports.clean = function clean () {
  * @return {Object} Gulp stream
  */
 exports.lint = function lint () {
-	// @todo [#2]: Lint HTML.
-	// - https://github.com/yvanavermaet/gulp-htmllint
-	// @todo [#7]: Lint Sass.
-	// - https://github.com/sasstools/gulp-sass-lint/
-	// - https://github.com/juanfran/gulp-scss-lint
-	// @todo [#4]: Lint SVG.
-	// - https://github.com/birjolaxew/svglint
-  return src(paths.javascript.lint)
-    .pipe(eslint(config.get('javascript.eslint')))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
+	const merged = merge(
+		// Lint HTML.
+		src(paths.html.lint)
+			.pipe(htmllint(config.get('html.htmllint'))),
+
+		// @todo [#7]: Lint Sass.
+		// - https://github.com/sasstools/gulp-sass-lint/
+		// - https://github.com/juanfran/gulp-scss-lint
+		// @todo [#4]: Lint SVG.
+		// - https://github.com/birjolaxew/svglint
+
+		// Lint JavaScript.
+		src(paths.javascript.lint)
+			.pipe(eslint(config.get('javascript.eslint')))
+			.pipe(eslint.format())
+			.pipe(eslint.failAfterError())
+	)
+
+	return merged.isEmpty() ? null : merged
 }
 
 /**
@@ -106,39 +117,104 @@ exports.lint = function lint () {
  * @return {Object} Gulp stream
  */
 exports.html = async function html () {
-  // Delete previously written HTML files.
-  del([paths.html.written])
+	// Delete previously written HTML files.
+	del([paths.html.written])
 
-  // Start SSG.
-  await ssg.init()
+	// Start SSG.
+	await ssg.init()
 
-  // Write HTML files.
-  await ssg.write()
+	// Write HTML files.
+	await ssg.write()
 
-  // Post-process HTML.
-  return src(paths.html.written)
+	// Post-process HTML.
+	return src(paths.html.written)
 		// @todo [#6]: Validate HTML after building.
 		// - https://github.com/validator/gulp-html
 		// - https://github.com/center-key/gulp-w3c-html-validator
-  	// @todo [#5]: Inline critical CSS.
-  	// Beautify HTML.
-    .pipe(beautify.html(config.get('build.html.beautify')))
-    // Minify HTML in production.
-    .pipe(gulpif(isProduction, htmlmin(config.get('build.html.htmlmin'))))
-    .pipe(dest(paths.build))
-    .pipe(connect.reload())
+		// @todo [#5]: Inline critical CSS.
+		// Beautify HTML.
+		.pipe(beautify.html(config.get('html.beautify')))
+		// Minify HTML in production.
+		.pipe(gulpif(isProduction, htmlmin(config.get('html.htmlmin'))))
+		.pipe(dest(paths.html.dest))
+		.pipe(connect.reload())
 }
 
-// @todo [#3]: Lint CSS after building.
-// @todo [#8]: Validate CSS after building.
-// - https://github.com/gchudnov/gulp-w3c-css
+/**
+ * Handle Sass tasks.
+ * Usage: `gulp sass`
+ *
+ * @since unreleased
+ *
+ * @return {Object} Gulp stream
+ */
+exports.sass = function sass (cb) {
+	console.log('@todo [#7]: Lint Sass.')
+	console.log('@todo: Process Sass.')
+	return cb()
+}
 
-// @todo [#11]: Bundle JavaScript modules.
-// @todo [#12]: Transpile modern JavaScript.
-// @todo [#13]: Polyfill modern JavaScript.
+/**
+ * Handle CSS tasks.
+ * Usage: `gulp css`
+ *
+ * @since unreleased
+ *
+ * @return {Object} Gulp stream
+ */
+exports.css = function css (cb) {
+	console.log('@todo: Post-process CSS.')
+	console.log('@todo [#3]: Lint CSS.')
+	console.log('@todo: Beautify CSS.')
+	console.log('@todo [#8]: Validate CSS.')
+	// - https://github.com/gchudnov/gulp-w3c-css
+	return cb()
+}
 
-// @todo [#9]: Optimize SVG.
-// @todo [#10]: Minify SVG.
+/**
+ * Handle JavaScript tasks.
+ * Usage: `gulp javascript`
+ *
+ * @since unreleased
+ *
+ * @return {Object} Gulp stream
+ */
+exports.javascript = function javascript (cb) {
+	console.log('@todo [#11]: Bundle JavaScript modules.')
+	console.log('@todo [#12]: Transpile modern JavaScript.')
+	console.log('@todo [#13]: Polyfill modern JavaScript.')
+	return cb()
+}
+
+/**
+ * Handle SVG tasks.
+ * Usage: `gulp svg`
+ *
+ * @since unreleased
+ *
+ * @return {Object} Gulp stream
+ */
+exports.svg = function svg (cb) {
+	console.log('@todo [#9]: Optimize SVG.')
+	console.log('@todo [#10]: Minify SVG.')
+	return cb()
+}
+
+/**
+ * Handle build tasks.
+ * Usage: `gulp build`
+ *
+ * @since unreleased
+ *
+ * @type {Object} Gulp series
+ */
+exports.build = series(
+	parallel(
+		exports.clean,
+		exports.lint
+	),
+	exports.html
+)
 
 /**
  * Handle version tasks.
@@ -149,79 +225,69 @@ exports.html = async function html () {
  * @return {Object} Gulp stream
  */
 exports.version = function version () {
-  const { version, repository } = require('./package.json')
-  const url = repository.url.replace('git+', '').replace('.git', '')
-  const [month, date, year] = new Date().toLocaleDateString('en-US').split('/')
-  const today = `${month}/${date}/${year}`
+	const { version, repository } = require('./package.json')
+	const url = repository.url.replace('git+', '').replace('.git', '')
+	const [month, date, year] = new Date().toLocaleDateString('en-US').split('/')
+	const today = `${month}/${date}/${year}`
 
-  /**
-   * Bump docblock version.
-   *
-   * @since unreleased
-   *
-   * @return {string} Bumped docblock version.
-   */
-  function bumpDocblock () {
-    return replace(/(?<!\))@since unreleased/g, `@since ${version}`)
-  }
+	/**
+	 * Bump docblock version.
+	 *
+	 * @since unreleased
+	 *
+	 * @return {string} Bumped docblock version.
+	 */
+	function bumpDocblock () {
+		return replace(/(?<!\))@since unreleased/g, `@since ${version}`)
+	}
 
-  const merged = merge(
-    // Config files.
-    src(paths.javascript.config)
-      .pipe(bumpDocblock())
-      .pipe(dest('./config/')),
+	const merged = merge(
+		// Config files.
+		src(paths.javascript.config)
+			.pipe(bumpDocblock())
+			.pipe(dest('./config/')),
 
-    // Source files.
-    src(paths.javascript.src)
-      .pipe(bumpDocblock())
-      .pipe(dest('./src/')),
+		// Source files.
+		src(paths.javascript.src)
+			.pipe(bumpDocblock())
+			.pipe(dest('./src/')),
 
-    // Root files.
-    src(paths.javascript.root.all)
-      .pipe(bumpDocblock())
-      .pipe(dest('./')),
+		// Root files.
+		src(paths.javascript.root.all)
+			.pipe(bumpDocblock())
+			.pipe(dest('./')),
 
-    // Changelog.
-    src(paths.changelog)
-      // Bump unreleased version.
-      .pipe(replace('## [Unreleased]', `## [${version}] - ${today}`))
-      // Remove empty changelog subheads.
-      .pipe(replace(
-        new RegExp(
-        	'### \(Added|Changed|Deprecated|Removed|Fixed|Security\)\\n\\n',
-        	'g'
-        ),
-        ''
-      ))
-      // Add default unreleased section.
-      .pipe(replace(
-        `## [${version}] - ${today}`,
-        '## [Unreleased]\n\n' +
-          '### Added\n\n' +
-          '### Changed\n\n' +
-          '### Deprecated\n\n' +
-          '### Removed\n\n' +
-          '### Fixed\n\n' +
-          '### Security\n\n' +
-          `## [${version}] - ${today}`
-      ))
-      // Bump unreleased link and add new release link.
-      .pipe(replace(
-        new RegExp('/compare/HEAD..\(HEAD\|\\d*\.\\d*\.\\d*\)', 'g'),
-        `/compare/HEAD..${version}\n[${version}]: ${url}/commits/${version}`)
-      )
-      .pipe(dest('./'))
-  )
+		// Changelog.
+		src(paths.changelog)
+			// Bump unreleased version.
+			.pipe(replace('## [Unreleased]', `## [${version}] - ${today}`))
+			// Remove empty changelog subheads.
+			.pipe(replace(
+				new RegExp(
+					'### \(Added|Changed|Deprecated|Removed|Fixed|Security\)\\n\\n',
+					'g'
+				),
+				''
+			))
+			// Add default unreleased section.
+			.pipe(replace(
+				`## [${version}] - ${today}`,
+				'## [Unreleased]\n\n' +
+					'### Added\n\n' +
+					'### Changed\n\n' +
+					'### Deprecated\n\n' +
+					'### Removed\n\n' +
+					'### Fixed\n\n' +
+					'### Security\n\n' +
+					`## [${version}] - ${today}`
+			))
+			// Bump unreleased link and add new release link.
+			.pipe(replace(
+				new RegExp('/compare/HEAD..\(HEAD\|\\d*\.\\d*\.\\d*\)', 'g'),
+				`/compare/HEAD..${version}\n[${version}]: ${url}/commits/${version}`)
+			)
+			.pipe(dest('./'))
+	)
 
-  return merged.isEmpty() ? null : merged
+	return merged.isEmpty() ? null : merged
 }
-
-/**
- * Builds the site.
- * Usage: `gulp build`
- *
- * @since unreleased
- *
- * @type {Object} Gulp series
- */
-exports.build = series(parallel(exports.clean, exports.lint), exports.html)
