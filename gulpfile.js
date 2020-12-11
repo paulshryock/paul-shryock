4cea00d48fbe97c840d11ddd55b40c5be4d7bd8e
Paul Shryock <paul@pshry.com>
2020-12-11 07:12:16 -0500

    #2: Fix spacing

--- gulpfile.js
+++ gulpfile.js
@@ -31,37 +31,37 @@ const isProduction = config.get('build.environment') === 'production'
  * @type {Object}
  */
 const paths = {
-  build: './build/pshry.com',
-  changelog: './CHANGELOG.md',
-  html: {
-  	src: './src/**/*.html',
-  	get lint () {
-  		return this.src
-  	},
-    written: './build/**/*.html',
-    dest: './build'
-  },
-  javascript: {
-    config: './config/*.js',
-    src: './src/**/*.js',
-    root: {
-      files: './*.js',
-      dotfiles: './.*.js',
-      get all () {
-        return [
-          this.files,
-          this.dotfiles
-        ]
-      }
-    },
-    get lint () {
-      return [
-        this.config,
-        this.src,
-        this.root.files
-      ]
-    }
-  }
+	build: './build/pshry.com',
+	changelog: './CHANGELOG.md',
+	html: {
+		src: './src/**/*.html',
+		get lint () {
+			return this.src
+		},
+		written: './build/**/*.html',
+		dest: './build'
+	},
+	javascript: {
+		config: './config/*.js',
+		src: './src/**/*.js',
+		root: {
+			files: './*.js',
+			dotfiles: './.*.js',
+			get all () {
+				return [
+					this.files,
+					this.dotfiles
+				]
+			}
+		},
+		get lint () {
+			return [
+				this.config,
+				this.src,
+				this.root.files
+			]
+		}
+	}
 }
 
 /**
@@ -73,9 +73,9 @@ const paths = {
  * @return {Promise}
  */
 exports.clean = function clean () {
-  return Promise.all([
-    del([paths.html.dest])
-  ])
+	return Promise.all([
+		del([paths.html.dest])
+	])
 }
 
 /**
@@ -99,13 +99,13 @@ exports.lint = function lint () {
 		// - https://github.com/birjolaxew/svglint
 
 		// Lint JavaScript.
-	  src(paths.javascript.lint)
-	    .pipe(eslint(config.get('javascript.eslint')))
-	    .pipe(eslint.format())
-	    .pipe(eslint.failAfterError())
+		src(paths.javascript.lint)
+			.pipe(eslint(config.get('javascript.eslint')))
+			.pipe(eslint.format())
+			.pipe(eslint.failAfterError())
 	)
 
-  return merged.isEmpty() ? null : merged
+	return merged.isEmpty() ? null : merged
 }
 
 /**
@@ -117,27 +117,27 @@ exports.lint = function lint () {
  * @return {Object} Gulp stream
  */
 exports.html = async function html () {
-  // Delete previously written HTML files.
-  del([paths.html.written])
+	// Delete previously written HTML files.
+	del([paths.html.written])
 
-  // Start SSG.
-  await ssg.init()
+	// Start SSG.
+	await ssg.init()
 
-  // Write HTML files.
-  await ssg.write()
+	// Write HTML files.
+	await ssg.write()
 
-  // Post-process HTML.
-  return src(paths.html.written)
+	// Post-process HTML.
+	return src(paths.html.written)
 		// @todo [#6]: Validate HTML after building.
 		// - https://github.com/validator/gulp-html
 		// - https://github.com/center-key/gulp-w3c-html-validator
-  	// @todo [#5]: Inline critical CSS.
-  	// Beautify HTML.
-    .pipe(beautify.html(config.get('html.beautify')))
-    // Minify HTML in production.
-    .pipe(gulpif(isProduction, htmlmin(config.get('html.htmlmin'))))
-    .pipe(dest(paths.html.dest))
-    .pipe(connect.reload())
+		// @todo [#5]: Inline critical CSS.
+		// Beautify HTML.
+		.pipe(beautify.html(config.get('html.beautify')))
+		// Minify HTML in production.
+		.pipe(gulpif(isProduction, htmlmin(config.get('html.htmlmin'))))
+		.pipe(dest(paths.html.dest))
+		.pipe(connect.reload())
 }
 
 /**
@@ -172,71 +172,71 @@ exports.css = function css (cb) {
  * @return {Object} Gulp stream
  */
 exports.version = function version () {
-  const { version, repository } = require('./package.json')
-  const url = repository.url.replace('git+', '').replace('.git', '')
-  const [month, date, year] = new Date().toLocaleDateString('en-US').split('/')
-  const today = `${month}/${date}/${year}`
+	const { version, repository } = require('./package.json')
+	const url = repository.url.replace('git+', '').replace('.git', '')
+	const [month, date, year] = new Date().toLocaleDateString('en-US').split('/')
+	const today = `${month}/${date}/${year}`
 
-  /**
-   * Bump docblock version.
-   *
-   * @since unreleased
-   *
-   * @return {string} Bumped docblock version.
-   */
-  function bumpDocblock () {
-    return replace(/(?<!\))@since unreleased/g, `@since ${version}`)
-  }
+	/**
+	 * Bump docblock version.
+	 *
+	 * @since unreleased
+	 *
+	 * @return {string} Bumped docblock version.
+	 */
+	function bumpDocblock () {
+		return replace(/(?<!\))@since unreleased/g, `@since ${version}`)
+	}
 
-  const merged = merge(
-    // Config files.
-    src(paths.javascript.config)
-      .pipe(bumpDocblock())
-      .pipe(dest('./config/')),
+	const merged = merge(
+		// Config files.
+		src(paths.javascript.config)
+			.pipe(bumpDocblock())
+			.pipe(dest('./config/')),
 
-    // Source files.
-    src(paths.javascript.src)
-      .pipe(bumpDocblock())
-      .pipe(dest('./src/')),
+		// Source files.
+		src(paths.javascript.src)
+			.pipe(bumpDocblock())
+			.pipe(dest('./src/')),
 
-    // Root files.
-    src(paths.javascript.root.all)
-      .pipe(bumpDocblock())
-      .pipe(dest('./')),
+		// Root files.
+		src(paths.javascript.root.all)
+			.pipe(bumpDocblock())
+			.pipe(dest('./')),
 
-    // Changelog.
-    src(paths.changelog)
-      // Bump unreleased version.
-      .pipe(replace('## [Unreleased]', `## [${version}] - ${today}`))
-      // Remove empty changelog subheads.
-      .pipe(replace(
-        new RegExp(
-        	'### \(Added|Changed|Deprecated|Removed|Fixed|Security\)\\n\\n',
-        	'g'
-        ),
-        ''
-      ))
-      // Add default unreleased section.
-      .pipe(replace(
-        `## [${version}] - ${today}`,
-        '## [Unreleased]\n\n' +
-          '### Added\n\n' +
-          '### Changed\n\n' +
-          '### Deprecated\n\n' +
-          '### Removed\n\n' +
-          '### Fixed\n\n' +
-          '### Security\n\n' +
-          `## [${version}] - ${today}`
-      ))
-      // Bump unreleased link and add new release link.
-      .pipe(replace(
-        new RegExp('/compare/HEAD..\(HEAD\|\\d*\.\\d*\.\\d*\)', 'g'),
-        `/compare/HEAD..${version}\n[${version}]: ${url}/commits/${version}`)
-      )
-      .pipe(dest('./'))
-  )
+		// Changelog.
+		src(paths.changelog)
+			// Bump unreleased version.
+			.pipe(replace('## [Unreleased]', `## [${version}] - ${today}`))
+			// Remove empty changelog subheads.
+			.pipe(replace(
+				new RegExp(
+					'### \(Added|Changed|Deprecated|Removed|Fixed|Security\)\\n\\n',
+					'g'
+				),
+				''
+			))
+			// Add default unreleased section.
+			.pipe(replace(
+				`## [${version}] - ${today}`,
+				'## [Unreleased]\n\n' +
+					'### Added\n\n' +
+					'### Changed\n\n' +
+					'### Deprecated\n\n' +
+					'### Removed\n\n' +
+					'### Fixed\n\n' +
+					'### Security\n\n' +
+					`## [${version}] - ${today}`
+			))
+			// Bump unreleased link and add new release link.
+			.pipe(replace(
+				new RegExp('/compare/HEAD..\(HEAD\|\\d*\.\\d*\.\\d*\)', 'g'),
+				`/compare/HEAD..${version}\n[${version}]: ${url}/commits/${version}`)
+			)
+			.pipe(dest('./'))
+	)
 
-  return merged.isEmpty() ? null : merged
+	return merged.isEmpty() ? null : merged
 }
 
 /**
