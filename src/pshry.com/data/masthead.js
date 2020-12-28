90c3097e9af73176d924ed948f3af4501be5cd5a
Paul Shryock <paul@pshry.com>
2020-12-28 11:25:39 -0500

    [#9/#10]: Optimize and minify SVG

--- src/pshry.com/data/masthead.js
+++ src/pshry.com/data/masthead.js
@@ -1,3 +1,4 @@
+const env = require('./env')
 const site = require('./site')
 
 /**
@@ -9,4 +10,10 @@ const site = require('./site')
  */
 module.exports = {
 	title: site.title,
+	logo: {
+		src: `/img/logos/ps-monogram-2021-4c${env.isProduction ? '.min' : ''}.svg`,
+		alt: 'Paul Shryock',
+		width: 36,
+		height: 36,
+	}
 }
