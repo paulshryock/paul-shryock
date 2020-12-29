b7eb79f03562463ba1569b23fa85663cd6d148e2
Paul Shryock <paul@pshry.com>
2020-12-29 15:10:32 -0500

    [#42/#60]: Add HTML head elements and favicons

--- src/pshry.com/data/site.js
+++ src/pshry.com/data/site.js
@@ -9,7 +9,19 @@ const config = require('config')
  */
 module.exports = {
 	lang: 'en-US',
+	csp: `default-src none;
+		style-src 'self' 'unsafe-inline';
+		script-src 'self' 'unsafe-inline';
+		img-src 'self';`,
+	referrer: 'strict-origin-when-cross-origin',
+	robots: 'noindex,nofollow',
 	title: 'Paul Shryock',
+	excerpt: '2021 reboot of my personal website.',
 	url: config.get('site.url'),
+	favicon: '/img/favicon/favicon',
+	colors: {
+		primary: '#0085ca',
+		white: '#ffffff',
+	},
 	year: new Date().getFullYear(),
 }
